36536accc06e507d4cd309cbd04adb97f4c8e448
Chinedu <chinedu@beyondit.it>
2023-07-21 16:01:52 +0100

    Base project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace newWebAppA00272195.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
